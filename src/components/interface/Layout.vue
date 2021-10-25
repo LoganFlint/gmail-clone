bbdd79667d757ef09e86539db4712ebb64594a6f
Danielle Smith <danielle.smith@launchbadge.com>
2021-10-25 15:58:22 -0700

    Started scaffolding layout.
    Signed-off-by: Danielle Smith <danielle.smith@launchbadge.com>

---
+++ src/components/interface/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Test
+    </div>
+</template>
+
+<script lang = "ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Layout",
+    components: {
+
+    },
+    props: {
+
+    },
+    setup(){
+        console.log("from Layout");
+    }
+
+});
+</script>
